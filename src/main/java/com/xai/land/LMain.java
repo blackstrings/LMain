26ae22a385c9b0492ab553a612884fec72d3e12d
xlao <xlao@IDS-DESIGN-2189.menards.net>
2016-10-17 09:31:36 -0500

    update

--- src/main/java/com/xai/land/LMain.java
+++ src/main/java/com/xai/land/LMain.java
@@ -4,10 +4,10 @@ public class LMain {
 
 	public static void main(String [] args){
 		
-		int total = LUtils.addFour(4);
-		String str = LUtils.getStr2();
+		//int total = LUtils.addFour(4);
+		//String str = LUtils.getStr2();
 		
-		System.out.println(total);
+		//System.out.println(total);
 		
 	}
 }
